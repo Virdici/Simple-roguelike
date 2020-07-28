c3d57f1f53aeda9e60afe70d5fe5fc46ef4c0cad
Virdici <44618762+Virdici@users.noreply.github.com>
2020-07-28 10:17:16 +0200

    Minor Generation change

--- Assets/Scripts/ModularGeneration/Generator.cs
+++ Assets/Scripts/ModularGeneration/Generator.cs
@@ -27,12 +27,10 @@ public class Generator : MonoBehaviour
     {
         if (collided == false)
         {
-
             dungeonContainter = GameObject.Find("DungeonContainer");
 
             var firstModule = (Module)Instantiate(startingModule, new Vector3(0, 0, 0), transform.rotation);
             firstModule.transform.SetParent(dungeonContainter.transform);
-
             var availableConnectors = new List<Connector>(firstModule.GetConnectors());
 
             for (int i = 0; i < size; i++)
@@ -65,48 +63,14 @@ public class Generator : MonoBehaviour
             SealEnds();
         }
     }
-    public static float AngleInRad(Vector3 vec1, Vector3 vec2)
-    {
-        return Mathf.Atan2(vec2.y - vec1.y, vec2.x - vec1.x);
-    }
 
-    //This returns the angle in degrees
-    public static float AngleInDeg(Vector3 vec1, Vector3 vec2)
-    {
-        return AngleInRad(vec1, vec2) * 180 / Mathf.PI;
-    }
     private void Connect(Connector startingObject, Connector ObjectToConnect)
     {
-        //var newModule = ObjectToConnect.transform.parent;
-
-
-
-
-        //var forwardVector = -startingObject.transform.forward;
-        //var angle1 = Vector3.Angle(Vector3.forward, forwardVector) * Mathf.Sign(forwardVector.x);
-        //var angle2 = Vector3.Angle(Vector3.forward, ObjectToConnect.transform.forward) * Mathf.Sign(ObjectToConnect.transform.forward.x);
-        //newModule.RotateAround(ObjectToConnect.transform.position, Vector3.up, angle1 - angle2);
-
-        //var correctPosition = startingObject.transform.position - ObjectToConnect.transform.position;
-        //newModule.transform.position += correctPosition;
-
-
-
-
-
-
-        //if (ObjectToConnect)
-        //{
-        //    Destroy(startingObject.gameObject);
-        //    Destroy(ObjectToConnect.gameObject);
-        //}
-
-
-
         var newModule = ObjectToConnect.transform.parent;
-        var forwardVector = -startingObject.transform.forward;
-        var correctedRotation = Azimuth(forwardVector) - Azimuth(ObjectToConnect.transform.forward);
-        newModule.RotateAround(ObjectToConnect.transform.position, Vector3.up, correctedRotation);
+        var objectToConnectVector = -startingObject.transform.forward;
+        var angle1 = Vector3.Angle(Vector3.forward, objectToConnectVector) * Mathf.Sign(objectToConnectVector.x);
+        var angle2 = Vector3.Angle(Vector3.forward, ObjectToConnect.transform.forward) * Mathf.Sign(ObjectToConnect.transform.forward.x);
+        newModule.RotateAround(ObjectToConnect.transform.position, Vector3.up, angle1 - angle2);
         var correctPosition = startingObject.transform.position - ObjectToConnect.transform.position;
         newModule.transform.position += correctPosition;
 
@@ -115,34 +79,22 @@ public class Generator : MonoBehaviour
             Destroy(startingObject.gameObject);
             Destroy(ObjectToConnect.gameObject);
         }
-
-
-
-
     }
     private void SealEnds()
     {
         var ends = FindObjectsOfType<Connector>();
-
         foreach (var end in ends)
         {
-
             var newSeal = (Module)Instantiate(Seal, new Vector3(2, UnityEngine.Random.Range(1, 400) * 30, 1), transform.rotation);
             var secondModuleConnectors = newSeal.GetConnectors();
             var connectorToConnect = secondModuleConnectors.FirstOrDefault(x => x.startingConnector) ?? secondModuleConnectors.ElementAt(UnityEngine.Random.Range(0, secondModuleConnectors.Length));
             Connect(end, connectorToConnect);
-
         }
     }
 
     private static TItem GetRandom<TItem>(TItem[] array)
     {
         return array[UnityEngine.Random.Range(0, array.Length)];
-    }
-
-    private static float Azimuth(Vector3 vector)
-    {
-        return Vector3.Angle(Vector3.forward, vector) * Mathf.Sign(vector.x);
     }
 
     public void RenewIfCollided()
