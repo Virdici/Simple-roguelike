d4f925193631b90b06461752e43dfed581ca38f8
Virdici <44618762+Virdici@users.noreply.github.com>
2020-07-28 16:14:30 +0200

    yup

--- Assets/GenerateNewLevel.cs
+++ Assets/GenerateNewLevel.cs
@@ -14,6 +14,7 @@ public class GenerateNewLevel : MonoBehaviour
 
     private void OnCollisionEnter(Collision col)
     {
+        //new generation on collision
         Debug.Log("collision detected" + col.contacts[0].point);
         generator.RenewIfCollided();
     }
