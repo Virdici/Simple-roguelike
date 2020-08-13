d3d442ee1efd7dd75333b13a9c2633a9ff56294d
Virdici <44618762+Virdici@users.noreply.github.com>
2020-08-13 14:23:34 +0200

    sync commit

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject wielder;
+
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.root.name);
+        other.transform.root.Find("Cube").GetComponent<Renderer>().material.color = Color.red;
+    }
+}
