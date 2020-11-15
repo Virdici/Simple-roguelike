c9c962b63144f4b10b3b9513ce95c59d93289274
Virdici <44618762+Virdici@users.noreply.github.com>
2020-11-15 15:04:08 +0100

    attack animation on player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,15 +9,20 @@ public class Player : MonoBehaviour
     public bool defeated = false;
     public int HP = 100;
     public int CurrentHP;
-   
+
+    public Animator animator;
+    PlayerMovement movement;
+
     void Start()
     {
         CurrentHP = HP;
+        movement = GetComponent<PlayerMovement>();
+
     }
 
     public void resetPosition()
     {
-            Debug.Log("???");
+        Debug.Log("???");
 
         transform.position = new Vector3(0, 1, 0);
 
@@ -29,8 +34,30 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(0, 1, 0);
 
         }
-    }
+        if (Input.GetMouseButtonDown(0))
+        {
+            //movement.isAttacking = true;
+            animator.SetBool("IsAttacking", true);
+            animator.SetTrigger("IsAttackingg");
+            //StartCoroutine(attack());
 
+
+
+            // movement.isAttacking = false;
+
+
+        }
+    }
+    IEnumerator attack()
+    {
+
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1f)
+            yield return null;
+
+        //movement.isAttacking = false;
+        animator.SetBool("IsAttacking", false);
+
+    }
     public void DealDamage(int damage)
     {
         if (this.CurrentHP >= 0)
